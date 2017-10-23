d6409bd60513a844466763625a1712a5ccc73d8f
Maximiliano Gigena <m.gigena@globant.com>
2017-10-23 02:36:39 -0300

    adding return to caller

--- app/src/main/java/com/example/mgigena/recievefromapp/MainActivity.java
+++ app/src/main/java/com/example/mgigena/recievefromapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mgigena.recievefromapp;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -24,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
             }else if(intent.getType().equals("text/plain")){
                 String message = bundle.getString(Intent.EXTRA_TEXT);
                 mTextView.setText(message);
+
+                Intent result = new Intent("com.example.RESULT_ACTION",Uri.parse("content://result_uri"));
+                setResult(Activity.RESULT_OK, result);
+                finish();
             }
         }
     }
