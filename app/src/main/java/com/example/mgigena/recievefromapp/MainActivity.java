ba117dd82f14f144eed81a7d11169a91475b5bbe
Maximiliano Gigena <m.gigena@globant.com>
2017-10-23 01:54:44 -0300

    first commit

---
+++ app/src/main/java/com/example/mgigena/recievefromapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mgigena.recievefromapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+
+        if(intent.getType() != null){
+            if(intent.getType().indexOf("image/") != -1){
+                //If it is an image, do something
+            }else if(intent.getType().equals("text/plain")){
+                String message = bundle.getString(Intent.EXTRA_TEXT);
+                mTextView.setText(message);
+            }
+        }
+    }
+}
